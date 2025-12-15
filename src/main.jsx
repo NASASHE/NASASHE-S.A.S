853f71df1f7770537dd5c22badc6c8e372073229
willys95 <64673489+willys95@users.noreply.github.com>
2025-12-14 23:38:25 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,6 @@
-// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-// import './index.css'
-
-import { BrowserRouter } from 'react-router-dom';
-
-// 1. ¡Importamos nuestro nuevo Proveedor de Contexto!
-import { CajaProvider } from './context/CajaContext';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* 2. Envolvemos TODA la aplicación (incluyendo el Router)
-           con el CajaProvider. Ahora CUALQUIER página
-           podrá acceder al estado de la "Base".
-    */}
-    <CajaProvider>
-      <BrowserRouter basename="/NASASHE-SAS">
-        <App />
-      </BrowserRouter>
-    </CajaProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <h1 style={{ color: "red" }}>Hola NASASHE</h1>
+);
