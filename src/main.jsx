486eb2310951749cf62bd08c86f201d3f7c8233e
Willy Rodriguez <rodriguezwilly4@gmail.com>
2025-12-16 17:29:39 -0500

    Use HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 // src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { HashRouter } from "react-router-dom";
+import { CajaProvider } from "./context/CajaContext";
 
-import { BrowserRouter } from 'react-router-dom';
-import { CajaProvider } from './context/CajaContext';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CajaProvider>
-      <BrowserRouter basename="/NASASHE-SAS">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CajaProvider>
   </React.StrictMode>
 );
