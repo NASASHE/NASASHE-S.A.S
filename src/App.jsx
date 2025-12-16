1e754a001c9ce80aa67ce41685a01a649e024e04
Willy Rodriguez <rodriguezwilly4@gmail.com>
2025-12-16 17:50:48 -0500

    Use HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,49 @@
 // src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Dashboard from './pages/Dashboard';
+import { Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
 
-import PaginaCompras from './pages/PaginaCompras';
-import PaginaGastos from './pages/PaginaGastos';
-import PaginaVentas from './pages/PaginaVentas';
-import PaginaVentasMenores from './pages/PaginaVentasMenores';
-
-import PaginaArticulos from './pages/PaginaArticulos';
-import PaginaProveedores from './pages/PaginaProveedores';
-import PaginaUsuarios from './pages/PaginaUsuarios';
-import PaginaConfiguracion from './pages/PaginaConfiguracion';
-
-import PaginaReportes from './pages/PaginaReportes';
-import PaginaRemisiones from './pages/PaginaRemisiones';
-
-import PaginaImpresion from './pages/PaginaImpresion';
+// Páginas
+import Dashboard from "./pages/Dashboard";
+import PaginaLogin from "./pages/PaginaLogin";
+import PaginaArticulos from "./pages/PaginaArticulos";
+import PaginaProveedores from "./pages/PaginaProveedores";
+import PaginaCompras from "./pages/PaginaCompras";
+import PaginaVentas from "./pages/PaginaVentas";
+import PaginaGastos from "./pages/PaginaGastos";
+import PaginaReportes from "./pages/PaginaReportes";
+import PaginaVentasMenores from "./pages/PaginaVentasMenores";
+import PaginaUsuarios from "./pages/PaginaUsuarios";
+import PaginaRemisiones from "./pages/PaginaRemisiones";
+import PaginaConfiguracion from "./pages/PaginaConfiguracion";
+import PaginaImpresion from "./pages/PaginaImpresion";
 
 function App() {
   return (
-    <>
-      <Header />
+    <Routes>
 
-      <Routes>
+      {/* 🔓 RUTA PÚBLICA */}
+      <Route path="/login" element={<PaginaLogin />} />
+
+      {/* 🔒 RUTAS PROTEGIDAS */}
+      <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
-
-        <Route path="/compras" element={<PaginaCompras />} />
-        <Route path="/gastos" element={<PaginaGastos />} />
-        <Route path="/ventas" element={<PaginaVentas />} />
-        <Route path="/ventas-menores" element={<PaginaVentasMenores />} />
-
         <Route path="/articulos" element={<PaginaArticulos />} />
         <Route path="/proveedores" element={<PaginaProveedores />} />
-        <Route path="/usuarios" element={<PaginaUsuarios />} />
-        <Route path="/configuracion" element={<PaginaConfiguracion />} />
-
+        <Route path="/compras" element={<PaginaCompras />} />
+        <Route path="/ventas" element={<PaginaVentas />} />
+        <Route path="/gastos" element={<PaginaGastos />} />
         <Route path="/reportes" element={<PaginaReportes />} />
+        <Route path="/ventas-menores" element={<PaginaVentasMenores />} />
+        <Route path="/usuarios" element={<PaginaUsuarios />} />
         <Route path="/remisiones" element={<PaginaRemisiones />} />
+        <Route path="/configuracion" element={<PaginaConfiguracion />} />
+      </Route>
 
-        <Route path="/imprimir" element={<PaginaImpresion />} />
-      </Routes>
+      {/* Ruta auxiliar */}
+      <Route path="/imprimir" element={<PaginaImpresion />} />
 
-      <Footer />
-    </>
+    </Routes>
   );
 }
 
