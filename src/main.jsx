906cef06bdff54a02ca41544c89ec9c29b4b8c49
Willy Rodriguez <rodriguezwilly4@gmail.com>
2025-12-16 14:00:47 -0500

    Replace HashRouter with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { CajaProvider } from './context/CajaContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <CajaProvider>
-      {/* HashRouter funciona perfecto en GitHub Pages + Tauri */}
-      <HashRouter>
+      <BrowserRouter basename="/NASASHE-SAS">
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </CajaProvider>
   </React.StrictMode>
 );
