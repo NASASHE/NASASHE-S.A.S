e2e1e8b5b71aff131d0ae0ff93b5766c897a7826
NASASHE <facturadornasashe@gmail.com>
2025-12-18 15:50:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/NASASHE-SAS/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/NASASHE-S.A.S/' : '/',
   plugins: [react()],
   resolve: {
     conditions: ['tauri'],
