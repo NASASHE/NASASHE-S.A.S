7948f349c5c4354f496cf448aa07cfaed1be2dc0
NASASHE <facturadornasashe@gmail.com>
2026-02-23 18:04:11 -0500

    Add files via upload

--- src/components/HeaderButton.jsx
+++ src/components/HeaderButton.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './HeaderButton.css';
 // 1. IMPORTAMOS 'Link'
 import { Link } from 'react-router-dom';
+import { resolveAssetPath } from '../utils/assetPath';
 
 // 2. AÑADIMOS 'to' A LAS PROPS
 function HeaderButton({ texto, icono, to }) {
+  const iconoResuelto = resolveAssetPath(icono);
 
   return (
     // 3. REEMPLAZAMOS 'a' POR 'Link' Y 'href' POR 'to'
     <Link to={to} className="header-button">
       
       <img 
-        src={icono} 
+        src={iconoResuelto} 
         alt={texto} 
         className="header-button-icon" 
       />
